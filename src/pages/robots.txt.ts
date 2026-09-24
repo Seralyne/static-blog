f8b52dca691f3df9f04066098bb57c2a92053335
Seralyne <serverboost100.dk@gmail.com>
2026-09-24 12:35:35 +0200

    Robots.txt wasn't allowing link previews, whoops.

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,6 +3,13 @@ import type { APIRoute } from "astro";
 const robotsTxt = `
 User-agent: *
 Disallow: /_astro/
+Allow: /_astro/*.png
+Allow: /_astro/*.jpg
+Allow: /_astro/*.jpeg
+Allow: /_astro/*.webp
+Allow: /_astro/*.avif
+Allow: /_astro/*.gif
+Allow: /_astro/*.svg
 
 Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
